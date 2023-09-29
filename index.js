8c06a15b050e044ea71f94be858e6b189a660bab
Abdur Rahim <abdurrahim300431@gmail.com>
2023-09-29 23:56:31 +0600

    Array Up

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var myArry= ["A", "B", "C", "D", "E", "F"];
 
-// var myArryPro = new Array ["A", "B", "C", "D", "E", "F"];
+var myArryPro = new Array ("A", "B", "C", "D", "E", "F")
 
-for(let item of myArry){
+for(let item of myArryPro){
     console.log(item);
 }
 

28d05252955cf9ad7beb87d74703b00ddaa9bde3
Abdur Rahim <abdurrahim300431@gmail.com>
2023-09-30 16:17:20 +0600

    Set Update

---
+++ ES6 jS/set.js
@@ -0,0 +1,16 @@
+var mySet = new Set();
+mySet.add("Bangladesh");
+mySet.add("India");
+mySet.add("Nepal");
+mySet.add("Chaina");
+mySet.add("Bhutan");
+mySet.add("Maldiv");
+
+// mySet.delete("Maldiv");
+// console.log(mySet.values());
+
+if(mySet.has("Canda")){
+    console.log("Yes")
+}else{
+    console.log("No")
+}
